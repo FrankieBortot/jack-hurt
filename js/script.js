6620ab0cd56ba6fae98072d9d2a86f06e85cbf3f
FrankieBortot <bortot.frankie@gmail.com>
2021-02-05 10:18:57 +0100

    up

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var JackEnergy;
+var JackGhostHand;
+var JackShots;
+var JackBook;
+
+var EnemyEnergy;
+var EnemyDamageRolls;
+
+function setJackEnergy() {
+      var JackEnergy = 123;
+      document.getElementById('Jack-Energy').innerHTML = JackEnergy;
+    }
