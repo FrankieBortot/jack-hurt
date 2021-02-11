c29d7d7fe67954ac9d8f454a7a9c6f0ecb1de039
FrankieBortot <bortot.frankie@gmail.com>
2021-02-11 19:10:58 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -6,7 +6,41 @@ var JackBook;
 var EnemyEnergy;
 var EnemyDamageRolls;
 
+var startingLog;
+
 function setJackEnergy() {
       var JackEnergy = 123;
       document.getElementById('Jack-Energy').innerHTML = JackEnergy;
     }
+
+
+
+function combatLog() {
+
+  var logElement = document.querySelector('#jackLog');
+
+  var clone = logElement.cloneNode(true);
+
+  clone.style.display = "flex";
+
+  var startingLog = document.querySelector('#firstLog');
+
+  startingLog.after(clone);
+  clone.order = 1;
+
+}
+
+function enemyLog() {
+
+  var logElement = document.querySelector('#enemyLog');
+
+  var clone = logElement.cloneNode(true);
+
+  clone.style.display = "flex";
+
+  var startingLog = document.querySelector('#firstLog');
+
+  startingLog.after(clone);
+  clone.order = 1;
+
+}
