9412d90e918886a5cdae894f3d1e60a30d3b1988
FrankieBortot <bortot.frankie@gmail.com>
2021-02-16 12:39:31 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,34 +1,102 @@
-var JackEnergy;
-var JackGhostHand;
-var JackShots;
-var JackBook;
+var JackEnergy = 100;
 
-var EnemyEnergy;
+var JackGhostHand = true;
+var JackGhostHandUse = 1;
+
+var JackGun = false;
+var JackGunShots = 0;
+
+var JackBook = true;
+
+var EnemyName;
+var EnemyEnergy = 100;
 var EnemyDamageRolls;
 
-var startingLog;
 
 function setJackEnergy() {
-  var JackEnergy = 123;
-  document.getElementById('Jack-Energy').innerHTML = JackEnergy;
+  document.getElementById('JackEnergy').innerHTML = JackEnergy;
 }
 
+function setJackItem() {
 
+  var item = document.getElementById('JackItem');
 
-// function jackLog() {
-//
-//   var logElement = document.querySelector('#jackLog');
-//
-//   var clone = logElement.cloneNode(true);
-//
-//   clone.style.display = "flex";
-//
-//   var startingLog = document.querySelector('#firstLog');
-//
-//   startingLog.after(clone);
-//   clone.order = 1;
-//
-// }
+  var value = document.createElement("div");
+    value.className += "value";
+    value.id = "JackItemValue";
+
+  var icon = document.createElement("div");
+    icon.className += "icon";
+
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  var use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+
+  if (JackGhostHand == true) {
+
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#ghosthand");
+
+    value.innerHTML = JackGhostHandUse;
+
+    svg.appendChild(use);
+    icon.appendChild(svg);
+    item.appendChild(value);
+    item.appendChild(icon);
+
+  } else if (JackGun == true) {
+
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#gun");
+
+    JackGunShots = 6;
+
+    value.innerHTML = JackGunShots;
+
+    svg.appendChild(use);
+    icon.appendChild(svg);
+    item.appendChild(value);
+    item.appendChild(icon);
+
+  } else if (JackBook == true) {
+
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#book");
+
+    svg.appendChild(use);
+    icon.appendChild(svg);
+    item.appendChild(icon);
+
+  }
+
+}
+
+function isJackDead() {
+  if (JackEnergy <= 0) {
+    return true;
+  }
+}
+
+function enemyNameIs(name) {
+  EnemyName = name;
+}
+
+function setEnemyName() {
+  document.getElementById('EnemyName').innerHTML = EnemyName;
+}
+
+function setEnemyEnergy(energy) {
+
+  if (energy == undefined) {
+    energy = EnemyEnergy;
+  }
+
+  EnemyEnergy = energy;
+  document.getElementById('EnemyEnergy').innerHTML = EnemyEnergy;
+
+}
+
+function isEnemyDead() {
+  if (EnemyEnergy <= 0) {
+    return true;
+  }
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -36,7 +104,15 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-function jackLog() {
+function jackLog(rolls, bonus) {
+
+  if (bonus == undefined) {
+    bonus = 0;
+  }
+
+  if (rolls == undefined) {
+    rolls = 2;
+  }
 
   var rolled = document.createElement("div");
     rolled.className += "rolled";
@@ -100,11 +176,13 @@ function jackLog() {
 
   var bonusDamage = document.createElement("div");
     bonusDamage.className += "bonus";
-    bonusDamage.innerHTML = "+ 10"
+    bonusDamage.innerHTML = "+ " + bonus ;
 
     dice.appendChild(firstIcon);
     dice.appendChild(secondIcon);
+    if (bonus > 0) {
     dice.appendChild(bonusDamage);
+    }
 
   var outcome = document.createElement("div");
     outcome.className += "outcome";
@@ -112,9 +190,11 @@ function jackLog() {
 
     rolled.appendChild(outcome);
 
+    var totalDamage = firstRoll + secondRoll + bonus;
+
   var total = document.createElement("div");
     total.className += "total";
-    total.innerHTML = firstRoll + secondRoll + 10;
+    total.innerHTML = totalDamage;
 
     outcome.appendChild(total);
 
@@ -124,14 +204,30 @@ function jackLog() {
 
   document.getElementById("combatLog").appendChild(rolled);
 
+  return totalDamage;
+
 }
 
-function enemyLog() {
+function enemyLog(bonus) {
+
+  if (bonus == undefined) {
+    bonus = 0;
+  }
 
   var rolled = document.createElement("div");
     rolled.className += "rolled";
     rolled.className += " enemy";
-    rolled.innerHTML = "Hai rollato"
+
+  var actor = document.createElement("div");
+    actor.className += "actor";
+
+  var name = document.createElement("div");
+    name.className += "name";
+    name.innerHTML = EnemyName;
+
+  actor.appendChild(name);
+  actor.appendChild(document.createTextNode("ha rollato"));
+  rolled.appendChild(actor);
 
   var dice = document.createElement("div");
     dice.className += "dice";
@@ -190,11 +286,13 @@ function enemyLog() {
 
   var bonusDamage = document.createElement("div");
     bonusDamage.className += "bonus";
-    bonusDamage.innerHTML = "+ 10"
+    bonusDamage.innerHTML = "+ " + bonus;
 
     dice.appendChild(firstIcon);
     dice.appendChild(secondIcon);
+    if (bonus > 0) {
     dice.appendChild(bonusDamage);
+    }
 
   var outcome = document.createElement("div");
     outcome.className += "outcome";
@@ -202,9 +300,11 @@ function enemyLog() {
 
     rolled.appendChild(outcome);
 
+    var totalDamage = firstRoll + secondRoll + bonus;
+
   var total = document.createElement("div");
     total.className += "total";
-    total.innerHTML = firstRoll + secondRoll + 10;
+    total.innerHTML = totalDamage;
 
     outcome.appendChild(total);
 
@@ -213,5 +313,72 @@ function enemyLog() {
     outcome.appendChild(damage);
 
   document.getElementById("combatLog").appendChild(rolled);
+
+  return totalDamage;
+}
+
+function combatold(jack, enemy) {
+
+  var jackDamage = jackLog(jack);
+  var enemyDamage = enemyLog(enemy);
+
+  JackEnergy -= enemyDamage;
+  EnemyEnergy -= jackDamage;
+
+  setJackEnergy();
+  setEnemyEnergy();
+
+  isJackDead();
+}
+
+function combat(jack,enemy) {
+  for (i = 0; i < 7; i++ ) {
+    if (i == 0) {
+      var jackDamage = jackLog(jack);
+      EnemyEnergy -= jackDamage;
+      setEnemyEnergy();
+
+      if (isEnemyDead() == true) {
+        break;
+      }
+
+    } else if (i == 4) {
+      var enemyDamage = enemyLog(enemy);
+
+      JackEnergy -= enemyDamage;
+      setJackEnergy();
+
+      if (isJackDead() == true) {
+        break;
+      }
+    }
+  }
+
+}
+
+function disableButton(button) {
+
+  document.getElementById(button).disabled = true;
+
+}
+
+function checkButtons() {
+
+  if (JackGhostHand == false || JackGhostHandUse == 0) {
+    document.getElementById("ghostHandAction").style.display = "none";
+  } else if (JackBook == false) {
+    document.getElementById("spellBookAction").style.display = "none";
+  } else if (JackGun == false || JackGunShots == 0) {
+    document.getElementById("gunAction").style.display = "none";
+  }
+
+}
+
+function usedGhostHand() {
+
+  JackGhostHandUse -= 1;
+  document.getElementById("JackItemValue").innerHTML = JackGhostHandUse;
+
+  document.getElementById("ghostHandAction").disabled = true;
 
 }
